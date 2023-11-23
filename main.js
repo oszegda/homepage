6335cae361b4fa2c22c199369e15cd361625a0a2
oszegda <110937320+oszegda@users.noreply.github.com>
2023-11-23 23:46:38 +0100

    added hidden message in empty div container

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-alert('WTF!');
-console.log('Hej, tu Ola!')
+
+console.log('Hej, tu Ola!');
+
+const firstName = 'Ola';
+const age = 25;
+
+console.log(`Nazywam sie ${firstName} i mam ${age} lat.`);
+
+const header = document.querySelector('.page-header__heading--js');
+
+header.innerHTML = `Nazywam sie ${firstName} i mam ${age} lat.`
+
+console.log(header.style)
+
+header.style.color = 'red'
+
+const hiddenMessage = document.querySelector('.hidden-message--js');
+
+hiddenMessage.innerHTML = 'To jest ukryta wiadomosc, ktora nie pojawia sie w glownym pliku html :)';
+
+hiddenMessage.style.color = 'hotpink'
